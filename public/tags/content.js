b8fb30bb2c7ca9d4e029705e1ed7c7c0456cef22
manickyoj <hotshot354@gmail.com>
2015-03-13 14:05:24 -0400

    Added upvote/downvote arrows and styled them.

--- public/tags/content.js
+++ public/tags/content.js
@@ -1,9 +1,15 @@
 <content>
     <a href={ this.link } class="post" each={ opts.posts }>
-        <img class="post-image" src={ this.imgSrc } />
-        <div class="post-content">
-           <h4 class="post-header">{ this.header }</h4>
-           <p class="post-line">{ this.content }</p>
+        <div class="vert-center">
+            <div class="arrows-wrapper">
+                <div class="arrow-box">&#x25B2;</div>
+                <div class="arrow-box">&#x25BC;</div>
+            </div>
+            <img class="post-image" src={ this.imgSrc } />
+            <div class="post-content">
+               <h4 class="post-header">{ this.header }</h4>
+               <p class="post-line">{ this.content }</p>
+            </div>
         </div>
     </a>
 
