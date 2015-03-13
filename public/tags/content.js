aa3593ab84f96cbb47c219d2603c58a6f8a21635
manickyoj <hotshot354@gmail.com>
2015-03-13 16:34:33 -0400

    Optimistically committing untested changes to render DB content to front end.

--- public/tags/content.js
+++ public/tags/content.js
@@ -1,15 +1,17 @@
 <content>
-    <div href={ link } class="post" each={ opts.posts }>
+
+    <div href={ this.url } class="post" each={ opts.posts }>
         <div class="vert-center">
             <div class="arrows-wrapper">
-                <div class="arrow-box upvote" onclick={ upvote } > &#x25B2; </div>
-                <div class="vote-count">{ this.votes }</div>
+                <div class="arrow-box upvote" onclick="" > &#x25B2; </div>
+                <div class="vote-count">{ this.count }</div>
                 <div class="arrow-box downvote" onclick={ downvote } > &#x25BC; </div>
             </div>
-            <img class="post-image" src={ this.imgSrc } />
+            <img class="post-image" src={ this.img } />
             <div class="post-content">
-               <h4 class="post-header">{ this.header }</h4>
-               <p class="post-line">{ this.content }</p>
+               <h4 class="post-header">{ this.title }</h4>
+               <h4 class="post-header"><small>{ this.byline }</small></h4>
+               <p class="post-line">{ this.summary }</p>
             </div>
         </div>
     </div>
