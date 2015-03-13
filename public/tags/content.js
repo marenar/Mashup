bb0658fc9dda27cc285ece42421f94e20e3c08e0
manickyoj <hotshot354@gmail.com>
2015-03-13 14:39:53 -0400

    Finished styling upvote and downvote arrows.

--- public/tags/content.js
+++ public/tags/content.js
@@ -2,8 +2,9 @@
     <a href={ this.link } class="post" each={ opts.posts }>
         <div class="vert-center">
             <div class="arrows-wrapper">
-                <div class="arrow-box">&#x25B2;</div>
-                <div class="arrow-box">&#x25BC;</div>
+                <div class="arrow-box upvote">&#x25B2;</div>
+                <div class="vote-count">{ this.votes }</div>
+                <div class="arrow-box downvote">&#x25BC;</div>
             </div>
             <img class="post-image" src={ this.imgSrc } />
             <div class="post-content">
