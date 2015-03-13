e01d3acd7a09841d8161c268120621941d05fa15
marenar <marena.richardson@students.olin.edu>
2015-03-13 15:32:52 -0400

    pull articles from feedly database

--- models/post.js
+++ models/post.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema ({
-	name: {type: String, required: true },
-    content: {type: String, required: true },
-    upvoteUsers: {[type: mongoose.Schema.Types.ObjectId, ref: 'User']},
-    downvoteUsers: {[type: mongoose.Schema.Types.ObjectId, ref: 'User']}
+	title: {type: String},
+    content: {type: String},
+    author: {type: String},
+    source: {type: String},
+    url: {type: String},
+    category: {type: String},
+    upvoteUsers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    downvoteUsers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    count: {type: Number}
 });
 
-module.exports.User = mongoose.model('Post', schema);
+module.exports = mongoose.model('Post', schema);
