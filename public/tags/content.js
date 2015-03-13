4ffaaa0e8b4bae0e0480c518ec2b5c646c6ebda1
manickyoj <hotshot354@gmail.com>
2015-03-13 03:33:26 -0400

    Front end mock-up data upgraded to riot-based data: still needs to be upgraded to back-end data.

---
+++ public/tags/content.js
@@ -0,0 +1,12 @@
+<content>
+    <a href={ this.link } class="post" each={ opts.posts }>
+        <img class="post-image" src={ this.imgSrc } />
+        <div class="post-content">
+           <h4 class="post-header">{ this.header }</h4>
+           <p class="post-line">{ this.content }</p>
+        </div>
+    </a>
+
+    // Javascript goes below
+
+</content>
---
+++ public/tags/sidebar.js
@@ -0,0 +1,5 @@
+<sidebar>
+    <div class="sidebar-category" each={ category in opts.categories }>
+        <div>{ category }</div>
+    </div>
+</sidebar>
